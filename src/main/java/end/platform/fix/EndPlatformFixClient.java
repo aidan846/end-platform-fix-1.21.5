dd1fbe3922008321bbb111914d2125dbd47ca196
Aidan <lando.aidan@gmail.com>
2025-05-17 12:23:18 -0400

    Initial commit

---
+++ src/main/java/end/platform/fix/EndPlatformFixClient.java
@@ -0,0 +1,10 @@
+package end.platform.fix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndPlatformFixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/end/platform/fix/EndPlatformFixDataGenerator.java
@@ -0,0 +1,11 @@
+package end.platform.fix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EndPlatformFixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
